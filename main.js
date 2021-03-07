df3664d20b571cb28f454596f8bd23b28cb4f722
abdoessordo <abdoessordo01@gmail.com>
2021-03-07 14:12:46 +0100

    created slider in home page

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 const menuIcon = document.getElementById("menu-icon");
 const slideoutMenu = document.getElementById("slideout-menu");
+const slides = document.getElementsByClassName("mySlides");
+const dots = document.getElementsByClassName("dot");
 
 menuIcon.addEventListener('click', function () {
   if (slideoutMenu.style.opacity == "1") {
@@ -10,3 +12,23 @@ menuIcon.addEventListener('click', function () {
     slideoutMenu.style.pointerEvents = 'auto';
   }
 })
+
+/////////////////////////////////////////
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" activedot", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " activedot";
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
+}
